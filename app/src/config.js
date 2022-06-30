19bbfcd07fec01ba51b9b8974255b12ef015755b
bamba-salim <52238083+bamba-salim@users.noreply.github.com>
2022-07-01 01:03:36 +0200

    Update config.js

--- app/src/config.js
+++ app/src/config.js
@@ -1,14 +1,6 @@
 export const Config = {
     name: "ShopList",
-    version: "1.0.0",
-    firebase_config: {
-        apiKey: "AIzaSyAodBowt1jXVv6d2r8xUwd3tMi9g1tUA8I",
-        authDomain: "shop-list-89803.firebaseapp.com",
-        projectId: "shop-list-89803",
-        storageBucket: "shop-list-89803.appspot.com",
-        messagingSenderId: "776480781029",
-        appId: "1:776480781029:web:a5e0dd47f1de5de3a2da0c"
-    }
+    version: "1.0.0"
 };
 
 export default Config;
